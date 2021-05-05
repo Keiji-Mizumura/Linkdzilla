088306ac09922de74b7c197cd12649271d45faa4
JPAlviso <joshua.alviso@gmail.com>
2021-05-05 17:40:41 +0800

    Hello my friend

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Linkdzilla</a> by wala lang
+          Welcome to <a href="https://nextjs.org">Linkdzilla</a> by wala lang sdfsdfsdfsdf
         </h1>
 
         <p className={styles.description}>
