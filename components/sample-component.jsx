814eb027869bf862068b0467f40c5f0a2508f7be
Keiji-Mizumura <keijimizumura@icloud.com>
2021-05-05 18:54:25 +0900

    another test :<

---
+++ components/sample-component.jsx
@@ -0,0 +1,10 @@
+function SampleComponent(){
+    return (
+        <div>
+            <h1>A BUTTON</h1>
+            <button>CLICK ME!</button>
+        </div>
+    )
+}
+
+export default SampleComponent;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+import SampleComponent from "../components/sample-component";
+
 function Home(){
   return(
     <div>
       <h1>Linkdzilla :D by linkdzilla.com</h1>
+      <SampleComponent />
+      <SampleComponent />
     </div>
   )
 }
